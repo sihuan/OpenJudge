08a82a77bbb329c2822b73fcf84168bce69bc652
SiHuan <sihuan@sakuya.love>
2019-11-11 18:54:01 +0800

    sihuan 1477

---
+++ sihuan/1477.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define OVERFLOW -1
+
+long long fibonacci(int n) {
+  long long preVal = 1;
+  long long prePreVal = 1;
+  if (n <= 1)
+    return 1;
+  else if (n > 91)
+    return -1;
+  long long loop = 1;
+  long long returnVal = 0;
+  while (loop < n) {
+    returnVal = preVal + prePreVal;
+    prePreVal = preVal;
+    preVal = returnVal;
+    loop++;
+  }
+  return returnVal;
+}
+//以上 submit
+
+int main() {
+  int n;
+  long long fib;
+  while (scanf("%d", &n) != EOF) {
+    fib = fibonacci(n);
+    if (fib == OVERFLOW)
+      puts("overflow");
+    else
+      printf("%lld\n", fib);
+  }
+  return 0;
+}
+/*
+AC
+*/
